42658ce8e03cbfb0c06d1f130e7b0b078620294e
PianoFF <Fangfei1star@gmail.com>
2019-11-24 12:26:13 +0000

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,29 +1,57 @@
 class OwnersController < ApplicationController
 
+  get '/?' do
+    redirect to :'/owners'
+  end 
+
   get '/owners' do
     @owners = Owner.all
     erb :'/owners/index' 
   end
 
   get '/owners/new' do 
+    @pets=Pet.all
     erb :'/owners/new'
   end
 
   post '/owners' do 
-    
+    @pets=Pet.all
+    #activerecord mass assignment. binding.pry to see the params value
+    #in it,it can be: pet_ids=>["1","2"]. ActiveRecord automatically assocaite the pets that have these ids to this owner.
+    @owner=Owner.create(params[:owner])
+
+    #if params["pet"]["name"] is not empty, then create this new pet instance and "push" it to update the relationship.
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+
+    redirect to :"owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do 
     @owner = Owner.find(params[:id])
+    @pets=Pet.all 
     erb :'/owners/edit'
   end
 
-  get '/owners/:id' do 
+  get '/owners/:id' do #show 
     @owner = Owner.find(params[:id])
+  
     erb :'/owners/show'
   end
 
   patch '/owners/:id' do 
-   
+
+    ###bug fix: if a user deselected everything, Sinatra doesn't generate an empty array for this key, thereofre, manually add it!
+    if !params[:owner].keys.include?("pet_ids")
+      params[:owner]["pet_ids"]=[]
+    end 
+
+    @owner=Owner.find(params[:id])
+    @owner.update(params[:owner])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect to :"owners/#{@owner.id}"
   end
 end
--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,26 +1,53 @@
 class PetsController < ApplicationController
 
+  
   get '/pets' do
     @pets = Pet.all
     erb :'/pets/index' 
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @pets=Pet.all 
+    @owners=Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    #mass assignment: creating new pet with association to an owner via owner_id. check the params via binding.pry
+    @pet=Pet.create(params[:pet])
+    # binding.pry
+    if params[:owner][:name]!=""
+      @owner=Owner.create(params[:owner])
+      @owner.pets<<@pet 
+    end
     redirect to "pets/#{@pet.id}"
   end
 
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
+    @owner=@pet.owner 
+    # binding.pry 
+    if @owner==nil
+      @owner_name="not owned yet"
+    else 
+      @owner_name=@owner.name 
+    end 
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  get '/pets/:id/edit'do
+    @pet=Pet.find(params[:id])
+    @owners=Owner.all 
 
+    erb :"pets/edit"
+  end
+  patch '/pets/:id' do 
+    @pet=Pet.find(params[:id])
+    @pet.update(params[:pet])
+    if params[:owner][:name]!=""
+      @owner=Owner.create(params[:owner])
+      @owner.pets<<@pet 
+    end
     redirect to "pets/#{@pet.id}"
   end
 end
